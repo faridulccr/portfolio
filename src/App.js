bca8a5dc463941c8bfdc34578b8911bea5be282f
Md Faridul islam <mdfaridulislamovi@gmail.com>
2022-04-27 01:20:41 +0600

    deployed

--- src/App.js
+++ src/App.js
@@ -9,10 +9,24 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Layout />}>
+        {/* <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
           <Route path="contact" element={<Contact />} />
+        </Route> */}
+
+        {/* it is for deployment */}
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+        </Route>
+        <Route path="/portfolio" element={<Layout />}>
+          <Route index element={<Home />} />
+        </Route>
+        <Route path="/about" element={<Layout />}>
+          <Route index element={<About />} />
+        </Route>
+        <Route path="/contact" element={<Layout />}>
+          <Route index element={<Contact />} />
         </Route>
       </Routes>
     </>
